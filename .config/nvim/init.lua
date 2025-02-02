261322c808408866fa725698da513ba298487d7e
Snake Sanders <elmarian22@gmail.com>
2025-02-02 17:25:29 +0100

    moved neotree to plugs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1034,16 +1034,6 @@ require('lazy').setup({
   --  NOTE: NeoTree
   {
     'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v3.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons',
-      'MunifTanjim/nui.nvim',
-    },
-    config = function()
-      -- ctr + b = toggles file explorer tree
-      vim.keymap.set('n', '<leader>b', ':Neotree filesystem toggle right<CR>', {})
-    end,
   },
   -- [[ end of custom plugins ]]
 }, {

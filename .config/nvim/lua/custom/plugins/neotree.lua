169d0a54c6111ef0977d8b174305e35c8d9a16c6
Snake Sanders <elmarian22@gmail.com>
2025-04-12 22:33:38 +0200

    removed duplicated neotreewq

--- .config/nvim/lua/custom/plugins/neotree.lua
+++ .config/nvim/lua/custom/plugins/neotree.lua
@@ -7,7 +7,19 @@ return {
     'MunifTanjim/nui.nvim',
   },
   config = function()
-    -- ctr + b = toggles file explorer tree
+    -- <leader>b = toggles file explorer tree
     vim.keymap.set('n', '<leader>b', ':Neotree filesystem toggle right<CR>', {})
+    -- \ = reveals current file in tree
+    vim.keymap.set('n', '\\', ':Neotree reveal<CR>', { silent = true })
   end,
-} 
+  opts = {
+    open_on_setup = false,
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+}

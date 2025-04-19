e6abf80eb0b7f1acf48bd74a3f5ff4e4a4d2b8f8
Snake Sanders <elmarian22@gmail.com>
2025-04-19 18:16:15 +0200

    disabled mini.comment

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
   -- disable buffer tabs
   { "akinsho/bufferline.nvim", enabled = false },
+  -- gc conflicts with mini comments gcc gcl
+  { "echasnovski/mini.comment", enabled = false },
 }

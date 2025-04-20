e10e9b6269a9690092b43e1a98440f5a11739f78
Snake Sanders <elmarian22@gmail.com>
2025-04-20 08:17:00 +0200

    disabled auto complete blink.cmp

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -3,4 +3,8 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   -- gc conflicts with mini comments gcc gcl
   { "echasnovski/mini.comment", enabled = false },
+  -- autocomplete is distracting
+  { "hrsh7th/nvim-cmp", enabled = false },
+  -- blink autocompletion
+  { "saghen/blink.cmp", enabled = false },
 }

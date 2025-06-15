bd0de484ca504dbf55ae80d2478f5513a36ffb7c
Snake Sanders <elmarian22@gmail.com>
2025-06-15 09:33:33 +0200

    add pomodoro to vim

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,13 @@
+-- config added because it is needed by Pomodoro
+return {
+  "nvim-lualine/lualine.nvim",
+  optional = true,
+  event = "VeryLazy",
+  opts = function(_, opts)
+    table.insert(opts.sections.lualine_x, 3, {
+      function()
+        return require("pomodoro").get_pomodoro_status("🍅❌", "🍅", "☕")
+      end,
+    })
+  end,
+}

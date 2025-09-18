2152dbc3b98463cef6e46d8eb7ecd827f4f73d2d
Snake Sanders <elmarian22@gmail.com>
2025-09-18 08:12:27 +0200

    add quiet to kanata launcher

--- .config/kanata/.kanata.sh
+++ .config/kanata/.kanata.sh
@@ -1,3 +1,5 @@
 # copy this file to $HOME directory and set the kbd file for your keybord.
 echo "changing keyboard layout to Home Row Mode"
-# sudo kanata --cfg ~/dotfiles/.config/kanata/macbookpro14.kbd
+
+# selecte the configuration to load
+# sudo kanata --quite --cfg ~/dotfiles/.config/kanata/macbookpro14.kbd

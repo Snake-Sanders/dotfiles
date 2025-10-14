b903a28929e152c332841ff3c2a887319b4a1439
Snake Sanders <elmarian22@gmail.com>
2025-10-14 15:31:28 +0200

    disabled pomodoro on start

--- .config/nvim/lua/plugins/pomodoro.lua
+++ .config/nvim/lua/plugins/pomodoro.lua
@@ -2,7 +2,7 @@ return {
   "quentingruber/pomodoro.nvim",
   lazy = false, -- needed so the pomodoro can start at launch
   opts = {
-    start_at_launch = true,
+    start_at_launch = false,
     work_duration = 25,
     break_duration = 5,
     delay_duration = 1, -- The additionnal work time you get when you delay a break

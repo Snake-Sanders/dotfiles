78c17f712f6a57bcd6c64b2a2a8fdd64ffc010da
Snake Sanders <elmarian22@gmail.com>
2025-11-01 19:59:53 +0100

    add codename goose to nvim

---
+++ .config/nvim/lua/plugins/goose-nvim.lua
@@ -0,0 +1,37 @@
+return {
+  "azorng/goose.nvim",
+  config = function()
+    require("goose").setup({
+      keymap = {
+        global = {
+          toggle = "<leader>it", -- Open goose. Close if opened
+          open_input = "<leader>ii", -- Opens and focuses on input window on insert mode
+          open_input_new_session = "<leader>iI", -- Opens and focuses on input window on insert mode. Creates a new session
+          open_output = "<leader>io", -- Opens and focuses on output window
+          toggle_focus = "<leader>if", -- Toggle focus between goose and last window
+          close = "<leader>iq", -- Close UI windows
+          toggle_fullscreen = "<leader>iF", -- Toggle between normal and fullscreen mode
+          select_session = "<leader>is", -- Select and load a goose session
+          goose_mode_chat = "<leader>imc", -- Set goose mode to `chat`. (Tool calling disabled. No editor context besides selections)
+          goose_mode_auto = "<leader>ima", -- Set goose mode to `auto`. (Default mode with full agent capabilities)
+          configure_provider = "<leader>ip", -- Quick provider and model switch from predefined list
+          diff_open = "<leader>id", -- Opens a diff tab of a modified file since the last goose prompt
+          diff_next = "<leader>in", -- Navigate to next file diff
+          diff_prev = "<leader>ip", -- Navigate to previous file diff
+          diff_close = "<leader>ic", -- Close diff view tab and return to normal editing
+          diff_revert_all = "<leader>ira", -- Revert all file changes since the last goose prompt
+          diff_revert_this = "<leader>irt", -- Revert current file changes since the last goose prompt
+        },
+      },
+    })
+  end,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        anti_conceal = { enabled = false },
+      },
+    },
+  },
+}

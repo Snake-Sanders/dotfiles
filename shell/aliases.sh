43a3ce6bd1e3b10212ffd0f37b79bbe0c1a800f8
Snake Sanders <elmarian22@gmail.com>
2026-03-01 13:18:16 +0100

    fix(zsh): add kb and tree to aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,5 +1,9 @@
+# remove aliases form zsh
 unalias ll 2>/dev/null
 unalias la 2>/dev/null
+
 ll() { /bin/ls -lh "$@"; }
 la() { /bin/ls -lah "$@"; }
 lla() { /bin/ls -lah "$@"; }
+tree() { command tree -L 2 "$@" }
+kb() { /bin/sh "$HOME/.kanata.sh" }

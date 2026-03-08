ecf91b768acf1844741cca1eb8110f554a01482d
Snake Sanders <elmarian22@gmail.com>
2026-03-08 18:36:45 +0100

    chore: ignore rust macro on rust analyzer

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -3,6 +3,19 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
+      servers = {
+        rust_analyzer = {
+          settings = {
+            ["rust-analyzer"] = {
+              procMacro = {
+                ignored = {
+                  ethosu_rs_macro = { "load_model" },
+                },
+              },
+            },
+          },
+        },
+      },
     },
   },
 }

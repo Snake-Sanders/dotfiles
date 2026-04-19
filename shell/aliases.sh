90bb4fcec0b218a360a670924a48bba0def78d7c
Snake Sanders <elmarian22@gmail.com>
2026-04-19 09:30:22 +0200

    chore: 'la' list files in compact format

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -3,7 +3,7 @@ unalias ll 2>/dev/null
 unalias la 2>/dev/null
 
 ll() { /bin/ls -lh "$@"; }
-la() { /bin/ls -lah "$@"; }
+la() { /bin/ls -ah "$@"; }
 lla() { /bin/ls -lah "$@"; }
 tree() { command tree -L 2 "$@" }
 kb() { /bin/sh "$HOME/.kanata.sh" }

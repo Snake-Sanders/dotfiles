03a01a2ee7ea2b9b5664170f98f403dcc2710586
Snake Sanders <elmarian22@gmail.com>
2026-07-04 19:45:06 +0200

    chore(nvim): explorer show hidden files

--- nvim/lua/plugins/explorer.lua
+++ nvim/lua/plugins/explorer.lua
@@ -1,8 +1,25 @@
 return {
   "folke/snacks.nvim",
   opts = {
+    picker = {
+      sources = {
+        files = {
+          hidden = true, -- Show dotfiles in file picker
+          ignored = true, -- Optional: show gitignored files in file picker
+        },
+        grep = {
+          hidden = true, -- Search inside hidden files
+          ignored = false, -- Optional: search inside gitignored files
+        },
+        explorer = {
+          hidden = true, -- Show dotfiles in explorer
+          ignored = false, -- Optional: show gitignored files in explorer
+        },
+      },
+    },
     explorer = {
       open_on_startup = false, -- 👈 this disables the auto open
+      auto_close = false,
     },
   },
 }

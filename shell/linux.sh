9f8f987fed54fc57d306f7f1b381df42cdbf3721
Snake Sanders <elmarian22@gmail.com>
2026-07-08 18:53:28 +0200

    chore(shell): add gdb aliases

--- shell/linux.sh
+++ shell/linux.sh
@@ -32,9 +32,35 @@ qwen3() {
   command ollama run qwen3:8b "$@"
 }
 
-jlink() {
-  command JLinkGDBServer -device AE302F80F55D5_HE "$@"
+jlink(){
+  local variant="$1"
+  shift
+  case "$variant" in
+    he) command JLinkGDBServer -device AE302F80F55D5_HE -if swd -RTTTelnetPort 19021 -port 3333 "$@" ;;
+    hp) command JLinkGDBServer -device AE302F80F55D5_HP -if swd -RTTTelnetPort 19022 -port 4444 "$@" ;;
+    *) echo "usage: jlink {he|hp}" >&2; return 1 ;;
+  esac
 }
+
+gdbma(){
+  local variant="$1"
+  shift
+  case "$variant" in
+    he) command gdb-multiarch cross/target/thumbv8m.main-none-eabihf/debug/vps-application "$@" ;;
+    hp) command gdb-multiarch cross/target/thumbv8m.main-none-eabihf/debug/vps-hp-application "$@" ;;
+    *) echo "usage: gdbma {he|hp}" >&2; return 1 ;;
+  esac
+}
+armgdb(){
+  local variant="$1"
+  shift
+  case "$variant" in
+    he) command arm-none-eabi-gdb cross/target/thumbv8m.main-none-eabihf/debug/vps-application "$@" ;;
+    hp) command arm-none-eabi-gdb cross/target/thumbv8m.main-none-eabihf/debug/vps-hp-application "$@" ;;
+    *) echo "usage: gdbma {he|hp}" >&2; return 1 ;;
+  esac
+}
+
 
 bat() {
   command batcat "$@"
